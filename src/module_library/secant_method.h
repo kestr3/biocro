9588f7f33f5f984946ec34a9dc8add0372098535
eloch216 <48919455+eloch216@users.noreply.github.com>
2025-03-26 17:07:06 -0500

    Remove secant_parameters struct

--- src/module_library/secant_method.h
+++ src/module_library/secant_method.h
@@ -7,34 +7,6 @@
 // Helper function declarations
 inline bool is_close(double x, double y, double atol, double rtol);
 inline bool is_zero(double x, double atol);
-/**
- * @struct A struct to hold parameters related to the secant method.
- *
- * @param check The value of the function at its last evaluation. If a
- *              root has been found, this should equal zero to within
- *              tolerance.
- * @param counter The number of iterations completed at termination of the secant
- *              method. Zero indicates no iterations. A value of 10
- *              indicates 10 iterations were completed.
- * @param max_iter The total number of iterations allowed. The secant method
- *              will terminate when this number is reached.
- * @param atol The absolute tolerance used to test for floating point number
- *              equality; primarily used to test if a zero has been found:
- *              Terminate if `abs(check) < atol`
- * @param rtol The relative tolerance used to test if iterates are not
- *              improving: Terminate if `abs(x0 - x1) <= atol + rtol * abs(x1)`
- */
-struct secant_parameters {
-    double check = 0;
-    int counter = 0;
-    const size_t max_iter = 100;
-    const double atol = 1e-12;
-    const double rtol = 1e-12;
-
-    secant_parameters(){};
-    secant_parameters(size_t max_iter, double atol, double rtol)
-        : check{0}, counter{0}, max_iter{max_iter}, atol{atol}, rtol{rtol} {};
-};
 
 /**
  * @brief Finds the zero of a function using the secant method.
@@ -83,76 +55,97 @@ struct secant_parameters {
  * The order of initial guesses can affect convergence, so make the second guess
  * the closer of the two if possible.
  *
- * @param [in] fun A function or object with `double operator()(double)` signature
- *             This routine is templated so any type with a call operator with
- *              the correct type signature will work.
- * @param [in] x0 The first guess.
- * @param [in] x1 The second guess.
- *             type signature secant method calls this.
- * @param [in] par The parameters for tolerances, maximum number of iterations, etc.
- *              See the `secant_parameters` documentation. These are passed by
- *             reference so the secant method routine can save the final function
- *              evaluation (as a check on accuracy) and the number of iterations.
+ * @param [in] fun A function or object with `double operator()(double)`
+ *             signature. This routine is templated so any type with a call
+ *             operator with the correct type signature will work.
  *
- * @return Approximate root. If the method fails, it will return it's last (and
- *      presumably best) guess for the root. If `check` member field in the
- *      secant_parameters object is not zero, then the method has failed.
- *      This routine will terminate if NaN or non-finite guesses are encountered.
+ * @param [in] x0 The first guess.
+ *
+ * @param [in] x1 The second guess.
+ *
+ * @param [in] max_iter The total number of iterations allowed. The secant
+ *             method will terminate when this number is reached.
+ *
+ * @param [in] atol The absolute tolerance used to test for floating point
+ *             number equality; primarily used to test if a zero has been found:
+ *             Terminate if `abs(check) < atol`
+ *
+ * @param [in] rtol The relative tolerance used to test if iterates are not
+ *             improving: Terminate if `abs(x0 - x1) <= atol + rtol * abs(x1)`
+ *
+ * @param [out] check The value of the function at its last evaluation. If a
+ *              root has been found, this should equal zero to within the
+ *              absolute tolerance.
+ *
+ * @param [out] counter The number of iterations completed at termination of the
+ *              secant method. For example, a value of zero indicates that no
+ *              iterations were needed, while a value of 10 indicates 10
+ *              iterations were completed.
+ *
+ * @return Approximate root. If the method fails, it will return its last (and
+ *         presumably best) guess for the root. If `check` is not zero, then the
+ *         method has failed. This routine will terminate if NaN or non-finite
+ *         guesses are encountered.
  */
 template <typename F>
 double find_root_secant_method(
-    F fun, double x0, double x1, secant_parameters& par)
+    F const fun,
+    double x0,
+    double x1,
+    size_t const max_iter,
+    double const atol,
+    double const rtol,
+    double& check,
+    size_t& counter)
 {
     // Check inputs for trivial solutions
     double y0 = fun(x0);
-    if (is_zero(y0, par.atol)) {
-        par.counter = 0;
-        par.check = y0;
+    if (is_zero(y0, atol)) {
+        counter = 0;
+        check = y0;
         return x0;
     }
 
     // Guesses should be different
-    if (is_close(x1, x0, par.atol, par.rtol)) {
+    if (is_close(x1, x0, atol, rtol)) {
         throw std::runtime_error(
-            "Within tolerance, x0 == x1. Initial guesses should be distinct. ");
+            "Within tolerance, x0 == x1. Initial guesses should be distinct.");
     }
 
     // Check second solution
     double y1 = fun(x1);
-    if (is_zero(y1, par.atol)) {
-        par.counter = 0;
-        par.check = y1;
+    if (is_zero(y1, atol)) {
+        counter = 0;
+        check = y1;
         return x1;
     }
 
-    for (size_t n = 1; n <= par.max_iter; ++n) {
+    for (size_t n = 1; n <= max_iter; ++n) {
         double secant_slope = (y1 - y0) / (x1 - x0);
 
-        // Secant Update Formula
+        // Secant update formula
         double x2 = x1 - y1 / secant_slope;
 
-        // Stop if divide by generates non finite value
-        // Shouldn't occur, except when y1 == y0 but x1 != x0
+        // Stop if the secant formula generates a non-finite value. This should
+        // not occur, except when y1 == y0 but x1 != x0.
         if (not std::isfinite(x2)) {
-            par.counter = n;
-            par.check = y1;
+            counter = n;
+            check = y1;
             return x1;
         }
 
         double y2 = fun(x2);
 
-        // Test convergence
-        // If y2 == 0, then x2 is a root
-        if (is_zero(y2, par.atol)) {
-            par.counter = n;
-            par.check = y2;
+        // Test convergence; if y2 == 0, then x2 is a root.
+        if (is_zero(y2, atol)) {
+            counter = n;
+            check = y2;
             return x2;
         }
-        // If no improvement in guesses, then terminate
-        // Prevents x1 == x0
-        if (is_close(x1, x2, par.atol, par.rtol)) {
-            par.counter = n;
-            par.check = y2;
+        // If no improvement in guesses, then terminate; prevents x1 == x0.
+        if (is_close(x1, x2, atol, rtol)) {
+            counter = n;
+            check = y2;
             return x2;
         }
 
@@ -163,8 +156,8 @@ double find_root_secant_method(
         y1 = y2;
     }
 
-    par.counter = par.max_iter;
-    par.check = y1;
+    counter = max_iter;
+    check = y1;
     return x1;
 }
 
