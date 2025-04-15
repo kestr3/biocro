97c4c1aa415e7c65237262dec4bab968fecd5e06
eloch216 <48919455+eloch216@users.noreply.github.com>
2025-04-15 18:16:57 -0500

    Add cumulative respiration

--- src/module_library/cumulative_carbon_dynamics.h
+++ src/module_library/cumulative_carbon_dynamics.h
@@ -9,15 +9,23 @@ namespace standardBML
 /**
  * @class cumulative_carbon_dynamics
  *
- * @brief Enables calculations of cumulative water dynamics.
+ * @brief Enables calculations of cumulative carbon dynamics.
  *
- * Cumulative water dynamics will be included in the simulation output as
+ * Cumulative canopy gas exchange will be included in the simulation output as
  * differential quantities called:
  * - ``'canopy_assimilation'``: The cumulative net CO2 assimilation
  * - ``'canopy_gross_assimilation'``: The cumulative gross CO2 assimilation
  * - ``'canopy_non_photorespiratory_CO2_release'``: The cumulative CO2 lost to
  *   non-photorespiratory CO2 release
  * - ``'canopy_photorespiration'``: The cumulative CO2 lost to photorespiration
+ *
+ * Cumulative maintenance respiration will be included in the simulation output
+ * as differential quantities called ``'Leaf_mr'``, ``'Stem_mr'``, etc, where
+ * "mr" standard for "maintenance respiration."
+ *
+ * Cumulative growth respiration will be included in the simulation output
+ * as differential quantities called ``'Stem_gr'``, ``'Root_gr'``, etc, where
+ * "gr" standard for "growth respiration."
  */
 class cumulative_carbon_dynamics : public differential_module
 {
@@ -32,12 +40,32 @@ class cumulative_carbon_dynamics : public differential_module
           canopy_gross_assimilation_rate{get_input(input_quantities, "canopy_gross_assimilation_rate")},
           canopy_photorespiration_rate{get_input(input_quantities, "canopy_photorespiration_rate")},
           canopy_RL_rate{get_input(input_quantities, "canopy_non_photorespiratory_CO2_release_rate")},
+          Grain_mrr{get_input(input_quantities, "Grain_mrr")},
+          Leaf_grr{get_input(input_quantities, "Leaf_grr")},
+          Leaf_mrr{get_input(input_quantities, "Leaf_mrr")},
+          Rhizome_grr{get_input(input_quantities, "Rhizome_grr")},
+          Rhizome_mrr{get_input(input_quantities, "Rhizome_mrr")},
+          Root_grr{get_input(input_quantities, "Root_grr")},
+          Root_mrr{get_input(input_quantities, "Root_mrr")},
+          Shell_mrr{get_input(input_quantities, "Shell_mrr")},
+          Stem_grr{get_input(input_quantities, "Stem_grr")},
+          Stem_mrr{get_input(input_quantities, "Stem_mrr")},
 
           // Get pointers to output quantities
           canopy_assimilation_op{get_op(output_quantities, "canopy_assimilation")},
           canopy_gross_assimilation_op{get_op(output_quantities, "canopy_gross_assimilation")},
           canopy_photorespiration_op{get_op(output_quantities, "canopy_photorespiration")},
-          canopy_RL_op{get_op(output_quantities, "canopy_non_photorespiratory_CO2_release")}
+          canopy_RL_op{get_op(output_quantities, "canopy_non_photorespiratory_CO2_release")},
+          Grain_mr_op{get_op(output_quantities, "Grain_mr")},
+          Leaf_gr_op{get_op(output_quantities, "Leaf_gr")},
+          Leaf_mr_op{get_op(output_quantities, "Leaf_mr")},
+          Rhizome_gr_op{get_op(output_quantities, "Rhizome_gr")},
+          Rhizome_mr_op{get_op(output_quantities, "Rhizome_mr")},
+          Root_gr_op{get_op(output_quantities, "Root_gr")},
+          Root_mr_op{get_op(output_quantities, "Root_mr")},
+          Shell_mr_op{get_op(output_quantities, "Shell_mr")},
+          Stem_gr_op{get_op(output_quantities, "Stem_gr")},
+          Stem_mr_op{get_op(output_quantities, "Stem_mr")}
     {
     }
     static string_vector get_inputs();
@@ -50,12 +78,32 @@ class cumulative_carbon_dynamics : public differential_module
     double const& canopy_gross_assimilation_rate;
     double const& canopy_photorespiration_rate;
     double const& canopy_RL_rate;
+    double const& Grain_mrr;
+    double const& Leaf_grr;
+    double const& Leaf_mrr;
+    double const& Rhizome_grr;
+    double const& Rhizome_mrr;
+    double const& Root_grr;
+    double const& Root_mrr;
+    double const& Shell_mrr;
+    double const& Stem_grr;
+    double const& Stem_mrr;
 
     // Pointers to output quantities
     double* canopy_assimilation_op;
     double* canopy_gross_assimilation_op;
     double* canopy_photorespiration_op;
     double* canopy_RL_op;
+    double* Grain_mr_op;
+    double* Leaf_gr_op;
+    double* Leaf_mr_op;
+    double* Rhizome_gr_op;
+    double* Rhizome_mr_op;
+    double* Root_gr_op;
+    double* Root_mr_op;
+    double* Shell_mr_op;
+    double* Stem_gr_op;
+    double* Stem_mr_op;
 
     // Main operation
     void do_operation() const;
@@ -67,7 +115,17 @@ string_vector cumulative_carbon_dynamics::get_inputs()
         "canopy_assimilation_rate",                      // Mg / ha / hr
         "canopy_gross_assimilation_rate",                // Mg / ha / hr
         "canopy_non_photorespiratory_CO2_release_rate",  // Mg / ha / hr
-        "canopy_photorespiration_rate"                   // Mg / ha / hr
+        "canopy_photorespiration_rate",                  // Mg / ha / hr
+        "Grain_mrr",                                     // Mg / ha / hr
+        "Leaf_grr",                                      // Mg / ha / hr
+        "Leaf_mrr",                                      // Mg / ha / hr
+        "Rhizome_grr",                                   // Mg / ha / hr
+        "Rhizome_mrr",                                   // Mg / ha / hr
+        "Root_grr",                                      // Mg / ha / hr
+        "Root_mrr",                                      // Mg / ha / hr
+        "Shell_mrr",                                     // Mg / ha / hr
+        "Stem_grr",                                      // Mg / ha / hr
+        "Stem_mrr"                                       // Mg / ha / hr
     };
 }
 
@@ -77,7 +135,17 @@ string_vector cumulative_carbon_dynamics::get_outputs()
         "canopy_assimilation",                      // Mg / ha / hr
         "canopy_gross_assimilation",                // Mg / ha / hr
         "canopy_non_photorespiratory_CO2_release",  // Mg / ha / hr
-        "canopy_photorespiration"                   // Mg / ha / hr
+        "canopy_photorespiration",                  // Mg / ha / hr
+        "Grain_mr",                                 // Mg / ha / hr
+        "Leaf_gr",                                  // Mg / ha / hr
+        "Leaf_mr",                                  // Mg / ha / hr
+        "Rhizome_gr",                               // Mg / ha / hr
+        "Rhizome_mr",                               // Mg / ha / hr
+        "Root_gr",                                  // Mg / ha / hr
+        "Root_mr",                                  // Mg / ha / hr
+        "Shell_mr",                                 // Mg / ha / hr
+        "Stem_gr",                                  // Mg / ha / hr
+        "Stem_mr"                                   // Mg / ha / hr
     };
 }
 
@@ -87,6 +155,16 @@ void cumulative_carbon_dynamics::do_operation() const
     update(canopy_gross_assimilation_op, canopy_gross_assimilation_rate);
     update(canopy_photorespiration_op, canopy_photorespiration_rate);
     update(canopy_RL_op, canopy_RL_rate);
+    update(Grain_mr_op, Grain_mrr);
+    update(Leaf_gr_op, Leaf_grr);
+    update(Leaf_mr_op, Leaf_mrr);
+    update(Rhizome_gr_op, Rhizome_grr);
+    update(Rhizome_mr_op, Rhizome_mrr);
+    update(Root_gr_op, Root_grr);
+    update(Root_mr_op, Root_mrr);
+    update(Shell_mr_op, Shell_mrr);
+    update(Stem_gr_op, Stem_grr);
+    update(Stem_mr_op, Stem_mrr);
 }
 
 }  // namespace standardBML
--- tests/testthat/test.cumulative_modules.R
+++ tests/testthat/test.cumulative_modules.R
@@ -1,5 +1,5 @@
-test_that('cumulative carbon and water dynamics can be added to crop models', {
-    res <- expect_silent(
+test_that('cumulative carbon and water dynamics can be added to the soybean model', {
+    soybean_res <- expect_silent(
         with(soybean, {run_biocro(
             c(initial_values, list(
                 canopy_assimilation = 0,
@@ -7,11 +7,137 @@ test_that('cumulative carbon and water dynamics can be added to crop models', {
                 canopy_non_photorespiratory_CO2_release = 0,
                 canopy_photorespiration = 0,
                 canopy_transpiration = 0,
+                Grain_mr = 0,
+                Leaf_gr = 0,
+                Leaf_mr = 0,
+                Rhizome_gr = 0,
+                Rhizome_mr = 0,
+                Root_gr = 0,
+                Root_mr = 0,
+                Shell_mr = 0,
                 soil_evaporation = 0,
+                Stem_gr = 0,
+                Stem_mr = 0,
                 total_precip = 0
             )),
             parameters,
             soybean_weather[['2002']],
+            direct_modules,
+            c(differential_modules, list(
+                'BioCro:cumulative_carbon_dynamics',
+                'BioCro:cumulative_water_dynamics'
+            ))
+        )})
+    )
+
+    # Check that all the assimilated carbon (gross assimilation) is balanced by
+    # the sum costs of photorespiration, non-photorespiratory CO2 release by the
+    # leaf, growth respiration, maintenance respiration, tissue growth, and
+    # litter formation.
+    soybean_res$total_carbon_use <- with(soybean_res, {
+        canopy_non_photorespiratory_CO2_release +
+        canopy_photorespiration +
+        (Grain - soybean$initial_values$Grain) +
+        Grain_mr +
+        (Leaf - soybean$initial_values$Leaf) +
+        Leaf_gr +
+        Leaf_mr +
+        (LeafLitter - soybean$initial_values$LeafLitter) +
+        (Rhizome - soybean$initial_values$Rhizome) +
+        Rhizome_gr +
+        Rhizome_mr +
+        (RhizomeLitter - soybean$initial_values$RhizomeLitter) +
+        (Root - soybean$initial_values$Root) +
+        Root_gr +
+        Root_mr +
+        (RootLitter - soybean$initial_values$RootLitter) +
+        (Shell - soybean$initial_values$Shell) +
+        Shell_mr +
+        (Stem - soybean$initial_values$Stem) +
+        Stem_gr +
+        Stem_mr +
+        (StemLitter - soybean$initial_values$StemLitter)
+    })
+
+    skip('Some carbon usage is not counted; skipping for now')
+
+    expect_equal(
+        soybean_res$canopy_gross_assimilation,
+        soybean_res$total_carbon_use
+    )
+})
+
+test_that('cumulative carbon and water dynamics can be added to the willow model', {
+    willow_res <- expect_silent(
+        with(willow, {run_biocro(
+            c(initial_values, list(
+                canopy_assimilation = 0,
+                canopy_gross_assimilation = 0,
+                canopy_non_photorespiratory_CO2_release = 0,
+                canopy_photorespiration = 0,
+                canopy_transpiration = 0,
+                Grain_mr = 0,
+                Leaf_gr = 0,
+                Leaf_mr = 0,
+                Rhizome_gr = 0,
+                Rhizome_mr = 0,
+                Root_gr = 0,
+                Root_mr = 0,
+                Shell_mr = 0,
+                soil_evaporation = 0,
+                Stem_gr = 0,
+                Stem_mr = 0,
+                total_precip = 0
+            )),
+            c(parameters, list(
+                Grain_mrr = 0,
+                Leaf_mrr = 0,
+                Rhizome_mrr = 0,
+                Root_mrr = 0,
+                Shell_mrr = 0,
+                Stem_mrr = 0
+            )),
+            weather[['2002']],
+            direct_modules,
+            c(differential_modules, list(
+                'BioCro:cumulative_carbon_dynamics',
+                'BioCro:cumulative_water_dynamics'
+            ))
+        )})
+    )
+})
+
+test_that('cumulative carbon and water dynamics can be added to the miscanthus model', {
+    willow_res <- expect_silent(
+        with(miscanthus_x_giganteus, {run_biocro(
+            c(initial_values, list(
+                canopy_assimilation = 0,
+                canopy_gross_assimilation = 0,
+                canopy_non_photorespiratory_CO2_release = 0,
+                canopy_photorespiration = 0,
+                canopy_transpiration = 0,
+                Grain_mr = 0,
+                Leaf_gr = 0,
+                Leaf_mr = 0,
+                Rhizome_gr = 0,
+                Rhizome_mr = 0,
+                Root_gr = 0,
+                Root_mr = 0,
+                Shell_mr = 0,
+                soil_evaporation = 0,
+                Stem_gr = 0,
+                Stem_mr = 0,
+                total_precip = 0
+            )),
+            c(parameters, list(
+                Grain_mrr = 0,
+                Leaf_mrr = 0,
+                Rhizome_mrr = 0,
+                Root_mrr = 0,
+                Shell_mrr = 0,
+                Stem_mrr = 0
+            )),
+            weather[['2002']],
             direct_modules,
             c(differential_modules, list(
                 'BioCro:cumulative_carbon_dynamics',
